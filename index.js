037095878fa56afe19f3ff0b9f2413edffed6811
Ali Madooei <madooei@jhu.edu>
2020-10-29 10:35:37 -0400

    Step 7

--- index.js
+++ index.js
@@ -5,4 +5,28 @@ const password = process.env.DB_ADMIN_PASSWORD;
 const dbname = "younote-db";
 const URI = `mongodb+srv://younote-admin:${password}@younote-api.cwpws.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
-console.log(password);
+const option = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+/*
+mongoose
+  .connect(URI, option)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+*/
+
+mongoose.connect(URI, option);
+
+mongoose.connection.on("open", () => {
+  console.log("Connected to MongoDB!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
