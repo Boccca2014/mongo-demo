2dff2bd4375aabb6fe2ed38f8d1895c21d555274
Ali Madooei <madooei@jhu.edu>
2020-10-29 10:31:45 -0400

    Step 6

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-const password = "not-shown-here";
+const password = process.env.DB_ADMIN_PASSWORD;
 const dbname = "younote-db";
-const URI = `mongodb+srv://younote-admin:${password}@younote-api.cwpws.mongodb.net/${dbname}?retryWrites=true&w=majority`
+const URI = `mongodb+srv://younote-admin:${password}@younote-api.cwpws.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+console.log(password);
