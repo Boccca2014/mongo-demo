4bcfe9762021f57ff4fb423d9006e9cfaa13bb49
Ali Madooei <madooei@jhu.edu>
2020-10-29 10:25:59 -0400

    Step 5

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const mongoose = require("mongoose");
 
-console.log(mongoose);
+const password = "not-shown-here";
+const dbname = "younote-db";
+const URI = `mongodb+srv://younote-admin:${password}@younote-api.cwpws.mongodb.net/${dbname}?retryWrites=true&w=majority`
